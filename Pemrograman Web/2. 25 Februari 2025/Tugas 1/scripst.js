5218ab4ba028216ffeed2d5f24f206978efcf5c4
SyaefulEffendi <160243849+SyaefulEffendi@users.noreply.github.com>
2025-03-12 02:34:35 +0700

    Delete Pemrograman Web directory

--- Pemrograman Web/2. 25 Februari 2025/Tugas 1/scripst.js
+++
@@ -1,9 +0,0 @@
-function getTextSignUp(){
-    myForm = document.formsignup;
-    alert("Hallo "+ myForm.nama.value + " akun anda berhasil di daftarkan");
-}
-
-function getTextSignIn(){
-    myForm = document.formsignin;
-    alert("Hallo "+ myForm.username.value + " Selamat Datang");
-}
--- Pemrograman Web/2. 25 Februari 2025/script.js
+++
@@ -1,4 +0,0 @@
-function getTextValue(){
-    var myForm = document.form1;
-    alert("Hallo "+myForm.nama.value+" selamat datang");
-}
