76742ce2c489ea01f846480b0214a9b935081bdd
SyaefulEffendi <160243849+SyaefulEffendi@users.noreply.github.com>
2025-03-12 02:35:06 +0700

    Delete Statistika dan Probabilitas directory

--- Statistika dan Probabilitas/1. 25 Februari 2025/main.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-data = {'Nama': ['Andi', 'Budi', 'Cici',
-        'Diana', 'Eka'],'Usia': [22, 23, 21, 25, 24]}
-df = pd.DataFrame(data)
-rata_rata = df['Usia'].mean()
-varians = df['Usia'].var()
-print("Rata-rata Usia:", rata_rata)
-print("Varians Usia:", varians)
